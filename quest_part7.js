ba31e5adf536af12e6f4a9bdf7850e081cefb39d
faizan4dev <faizan4dev@gmail.com>
2026-03-07 23:09:58 +0500

    solutions to assignemnt

---
+++ quest_part7.js
@@ -0,0 +1,38 @@
+// Q01 : average of array elements
+// const arrayavg = (arr) => {
+//     let sum = 0;
+//     // sum = parseInt(sum);
+//     for(let i=0;i<arr.length;i++){
+//         sum+=parseInt(arr[i]);
+//     }
+//     return sum/arr.length;
+// }
+
+// let arr = [];
+// let itr = 5;
+// let num;
+// for(let j=0;j<itr;j++){
+//     num = prompt(`Enter number ${j} :`);
+//     arr.push(num);
+// }
+
+// let add = arrayavg(arr);
+// console.log(`Average of the given numbers is : ${add}`)
+
+// Q2 :even or odd
+const chkeven = (numb) =>{
+    if(numb%2 ===0){
+        return 'even';
+    }
+    else{
+        return 'odd';
+    }
+}
+
+let evn = chkeven(5);
+console.log(evn);
+
+// or 
+const chkeven1 = (numb) =>{ return (numb%2 ===0) ? 'even' : 'odd'; };
+let evn1 = chkeven1(5);
+console.log(evn1);
