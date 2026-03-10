94020fac62db0d810733bd1969ace0208486b5a3
faizan4dev <faizan4dev@gmail.com>
2026-03-10 12:27:16 +0500

    map

--- array_methds(ii).js
+++ array_methds(ii).js
@@ -8,6 +8,28 @@ let print = function(el){
 arr.forEach(print);
 
 // or 
-arr.forEach(el = () =>{
+arr.forEach(el =>{
     console.log()
 } );
+
+// Map function : accesses each element and return a new array with the result of the function applied to each element
+let nums = [1,2,3,4];
+
+let doubled = nums.map(n => n * 2);
+
+console.log(doubled);
+
+// to upper case
+names = ['Ali',"sudais","nauman"];
+
+let upName =names.map(nam => nam.toUpperCase());
+console.log(upName);
+
+// use in dictionary
+let users = [
+    {name:"Ali", age:20},
+    {name:"Sara", age:25}
+];
+
+let namm = users.map(user => user.name);
+console.log(namm);
