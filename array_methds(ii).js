d6db2c927973a24ec4831370226f433a4f3f1e7d
faizan4dev <faizan4dev@gmail.com>
2026-03-11 22:53:52 +0500

    filter

--- array_methds(ii).js
+++ array_methds(ii).js
@@ -41,3 +41,8 @@ function square(n){
 }   
 let squared = nums.map(square);
 console.log(squared);
+
+// filter method : creates a new array with all elements that pass the condition implemented by the provided function
+let numbers = [1,2,3,4,5,6];
+let evenNumbers = numbers.filter(n => n % 2 === 0);
+console.log(evenNumbers);
