7bbab0510d22d267a542f1e9de49e8c31ec2c20b
faizan4dev <faizan4dev@gmail.com>
2026-03-11 23:03:55 +0500

    filter strings

--- array_methds(ii).js
+++ array_methds(ii).js
@@ -46,3 +46,8 @@ console.log(squared);
 let numbers = [1,2,3,4,5,6];
 let evenNumbers = numbers.filter(n => n % 2 === 0);
 console.log(evenNumbers);
+
+// for strings
+let words = ["cat","elephant","dog","giraffe"];
+let longWords = words.filter(word => word.length <= 3);
+console.log(longWords);
