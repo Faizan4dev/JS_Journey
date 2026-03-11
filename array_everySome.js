cf4d55ec5c5d704f124ee38d572c44d370f3b4f3
faizan4dev <faizan4dev@gmail.com>
2026-03-11 23:35:47 +0500

    every

---
+++ array_everySome.js
@@ -0,0 +1,7 @@
+let nums = [1, 2, 3, 4, 5];
+let evenchk = nums.every(num => num%2 == 0);
+console.log(evenchk);
+
+let evens = [0,2,4,6,8];
+let evenchek = evens.every(even => even%2 == 0);
+console.log(evenchek);
