32be3e5c588099b6fb48b18ccb87107abdc80de8
faizan4dev <faizan4dev@gmail.com>
2026-03-11 23:41:25 +0500

    some method

--- array_everySome.js
+++ array_everySome.js
@@ -1,3 +1,4 @@
+// Every
 let nums = [1, 2, 3, 4, 5];
 let evenchk = nums.every(num => num%2 == 0);
 console.log(evenchk);
@@ -5,3 +6,10 @@ console.log(evenchk);
 let evens = [0,2,4,6,8];
 let evenchek = evens.every(even => even%2 == 0);
 console.log(evenchek);
+
+// Some
+let somechk = nums.some(num => num%2 == 0);
+console.log(somechk);
+
+let somechk2 = nums.some(num => num%2 == 1);
+console.log(somechk2);
