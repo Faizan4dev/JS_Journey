d9506cf15172a0e70d40b60e1c6c757b9ccd2945
faizan4dev <faizan4dev@gmail.com>
2026-03-15 23:47:16 +0500

    destructring

---
+++ destructring.js
@@ -0,0 +1,17 @@
+let names = ["John", "Doe", "Smith", "Jane", "Doe"];
+// Destructuring assignment : assigning array elements to variables , instead of assigning
+// them one by one we can assign them using destructring assignment 
+let [fname , lname] = names;
+console.log(fname);
+console.log(lname);
+
+// we can skip an element by using space bw commas
+let[tname, ,zname] = names;
+console.log(tname);
+console.log(zname);  //so it'll print john smith instead of john doe smith
+
+// it can also use rest operator and that stores the remaining elements in an array
+let [firstName , lastName , ...rest] = names;
+console.log(firstName);
+console.log(lastName);
+console.log(rest);  //so it'll store the remaining elements as array
