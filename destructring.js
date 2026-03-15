e3ae732f078532c2a4d49c1892b5c99af74a43c7
faizan4dev <faizan4dev@gmail.com>
2026-03-16 00:21:03 +0500

    object dest

--- destructring.js
+++ destructring.js
@@ -1,3 +1,5 @@
+// ********* Array Destructuring *********
+
 let names = ["John", "Doe", "Smith", "Jane", "Doe"];
 // Destructuring assignment : assigning array elements to variables , instead of assigning
 // them one by one we can assign them using destructring assignment 
@@ -15,3 +17,19 @@ let [firstName , lastName , ...rest] = names;
 console.log(firstName);
 console.log(lastName);
 console.log(rest);  //so it'll store the remaining elements as array
+
+// ********* Object Destructuring *********
+let person = {
+    name : "John",
+    age : 30,
+    city : "New York"
+};
+//in object dest we use keys as variable names :
+let {name , age} = person;
+console.log(name);
+console.log(age);
+
+// to use another variable name we can use :
+let {name : fname1 , age : age1} = person;
+console.log(fname1);
+console.log(age1); 
