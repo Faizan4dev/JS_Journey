72cb8975d40dbed16ce3c9c9f49d277f856fafe9
faizan4dev <faizan4dev@gmail.com>
2026-03-15 22:54:49 +0500

    rest operator

---
+++ rest_operator.js
@@ -0,0 +1,8 @@
+// Rest operator : it collects the numbers into a single iterable
+
+// for example in functions it takes arguments and stores them into array
+
+function sum(...arg){
+    console.log(arg);
+}
+sum(1,2,3);
