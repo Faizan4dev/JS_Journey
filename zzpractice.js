0167a08f282c9498f4bb475e95777ad8cd7afcb0
faizan4dev <faizan4dev@gmail.com>
2026-04-14 22:08:12 +0500

    Practice mid term js

---
+++ zzpractice.js
@@ -0,0 +1,42 @@
+let names = ["ali", "Amal", "Saher"];
+let upr = names.map((name) => name.toUpperCase());
+console.log(upr);
+
+let str = ["a", "P", "S"];
+let add = names.map((name) => name + "bb");
+console.log(add);
+
+// filter
+let nums = [1, 2, 3, 4, 5, 6];
+let odd = nums.filter((num) => num % 2 !== 0);
+console.log(odd);
+
+let words = ["cat", "elephant", "dog", "giraffe"];
+let long = words.filter((word) => word.length > 3);
+console.log(long);
+
+// reduce
+let sum = nums.reduce((res, pon) => res + pon);
+console.log(sum);
+
+let max = nums.reduce((acc, resl) => (resl > acc ? resl : acc));
+console.log(max);
+let min = nums.reduce((acc, resl) => (resl < acc ? resl : acc));
+console.log(min);
+
+// spread operator
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+let comb = [...arr1, ...arr2, ...arr1];
+console.log(comb);
+
+let stri = "Hello";
+let chars = [...stri.toLocaleUpperCase()];
+console.log(chars);
+
+// rest operator
+function sumAll(...numbers) {
+  return numbers.reduce((acc, num) => acc + num, 0);
+}
+let arr = [1, 2, 3, 3];
+console.log(sumAll(...arr));
