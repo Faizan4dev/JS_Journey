d48d6eda62cd06f70c8b67fb13b46af132d00a2e
faizan4dev <faizan4dev@gmail.com>
2026-04-22 23:55:26 +0500

    Append,Prenpend and...

--- DOM/dom2_navig.js
+++ DOM/dom2_navig.js
@@ -7,6 +7,15 @@ h4.nextElementSibling.style.color = "red";
 let ul = document.querySelector("ul");
 ul.children[1].style.color = "Yellow";
 
-// Adding Elements
+// ------- Adding Elements -----------
 let parah = document.createElement("p");
 parah.innerText = "This is a new Paragraph added by JS manipulation";
+let box = document.querySelector(".box");
+box.appendChild(parah); //appended inside the box div
+// append
+parah.append("This is the 2nd line appneded!");
+let btn = document.createElement("button");
+btn.innerText = "Click me!";
+parah.append(btn);
+// prepend
+let line = parah.prepend("This is the 1st line prepended! ");
